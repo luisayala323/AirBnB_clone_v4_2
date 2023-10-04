4c1aa4dc2ff05b08934d2d27e2143ebbaacf39fa
Luis <luisayala323@gmail.com>
2023-10-04 12:15:41 -0400

    .py files

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,6 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -38,10 +39,12 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
+
                            places=places,
                            cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
     """ Main Function """
+
     app.run(host='0.0.0.0', port=5000)
